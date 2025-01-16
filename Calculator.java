41837058b4dfba443f9078a2b77d2dfad5f9b8b3
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-16 23:28:02 +0530

    calculator class

--- Calculator.java
+++ Calculator.java
@@ -3,25 +3,17 @@ class Calculator{
 
 	int addition(int[] numbers){
 		
-		//int[] numbers;
-		//UserInput input = new UserInput();
-	
-		//numbers = input.userInput();
 
 		int sum = numbers[0] + numbers[1];
-		
+	
 		return sum;
 
 
 	}
 
 
-	int substraction(int [] numbers){
+	int subtraction(int [] numbers){
 		
-		//int[] numbers;
-		//UserInput input = new UserInput();
-	
-		//numbers = input.userInput();
 
 		int sum = numbers[0] - numbers[1];
 		
@@ -43,14 +35,46 @@ class Calculator{
 	}
 
 
-	int division(int numbers[]){
+	int division(int[] numbers) {
+        
 		
-
-		int sum = numbers[0] / numbers[1];
+		if (numbers[1] == 0) {
+            System.out.println(" Division by zero is invalid");
+            return 0;
+        }
+        
 		
-		return sum;
+		return numbers[0] / numbers[1];
+    }
 
 
-	}
+	// Generates Fibonacci sequence up to n terms
+    void fibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print(a + " " + b);
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+        System.out.println();
+    }
 
+    // Calculates the sum of an array
+    int sumOfArray(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    // Calculates the mean of an array
+    double meanOfArray(int[] array) {
+        int sum = sumOfArray(array);
+        return (double) sum / array.length;
+    }
 }
+
+
