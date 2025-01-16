e564497751b8632e9e375b9867e0ac893371cc0b
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-16 23:29:33 +0530

    user input class

--- UserInput.java
+++ UserInput.java
@@ -24,4 +24,52 @@ class UserInput{
 	}
 
 
+
+	// Returns a single number (e.g., for Fibonacci sequence)
+    int singleNumberInput() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        return scan.nextInt();
+    }
+
+
+	// Returns an array of numbers for operations like sum of array and mean
+    int[] arrayInput() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int size = scan.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = scan.nextInt();
+        }
+
+        return array;
+    }
+
+
+    // Displays menu and gets the user's choice for calculation
+    int getCalculationChoice() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Fibonacci Sequence");
+        System.out.println("6. Sum of Array");
+        System.out.println("7. Mean of Array");
+
+        System.out.println("Select a calculation : ");
+        return scan.nextInt();
+  
+  
+    }
+
+
 }
+
+
