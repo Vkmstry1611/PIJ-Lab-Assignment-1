0be18ab8cfde687b3cc1b07b6c193d64743b06c8
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2025-01-16 23:31:05 +0530

    Main

--- Main.java
+++ Main.java
@@ -1,24 +1,68 @@
-// Name : Vidish ketankumar Mistry
+// Name : Vidish Ketankumar Mistry
 // PRN : 23070126146 
 // Batch  : B3 
 
 
-class Main{
 
-	public static void main(String args[]){
 
-		
-		int[] numbers;
-		UserInput input = new UserInput();
-	
-		numbers = input.userInput();
+// Main program for the Calculator
+class Main {
 
-		
-		Calculator cal = new Calculator();
-		System.out.println("Sum is : "+ cal.addition(numbers));
+    public static void main(String[] args) {
+        // Create UserInput instance to handle input
+        UserInput input = new UserInput();
 
-		System.out.println("Difference is : "+cal.substraction(numbers));
-	
-	}
+        // Let user select calculation type
+        int choice = input.getCalculationChoice();
 
+        Calculator cal = new Calculator();
+
+        switch (choice) {
+            case 1:
+                // Addition
+                int[] numbers = input.userInput();
+                System.out.println("Sum is: " + cal.addition(numbers));
+                break;
+
+            case 2:
+                // Subtraction
+                numbers = input.userInput();
+                System.out.println("Difference is: " + cal.subtraction(numbers));
+                break;
+
+            case 3:
+                // Multiplication
+                numbers = input.userInput();
+                System.out.println("Product is: " + cal.multiplication(numbers));
+                break;
+
+            case 4:
+                // Division
+                numbers = input.userInput();
+                System.out.println("Quotient is: " + cal.division(numbers));
+                break;
+
+            case 5:
+                // Fibonacci Sequence
+                int n = input.singleNumberInput();
+                System.out.println("Fibonacci sequence: ");
+                cal.fibonacci(n);
+                break;
+
+            case 6:
+                // Sum of Array
+                int[] array = input.arrayInput();
+                System.out.println("Sum of array: " + cal.sumOfArray(array));
+                break;
+
+            case 7:
+                // Mean of Array
+                array = input.arrayInput();
+                System.out.println("Mean of array: " + cal.meanOfArray(array));
+                break;
+
+            default:
+                System.out.println("Invalid choice!");
+        }
+    }
 }
